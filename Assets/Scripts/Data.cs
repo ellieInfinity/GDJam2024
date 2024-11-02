85992f1198da632e4110f71a598b9bb827f7278d
Jayden Deller-Daoust <jayden.dellerdaoust@gmail.com>
2024-11-02 15:20:11 -0400

    Important Tetris Stuff

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,16 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Data : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static readonly float cos = Mathf.Cos(Mathf.PI / 2f);
+    public static readonly float sin = Mathf.Sin(Mathf.PI / 2f);
+    public static readonly float[] RotationMatrix = new float[] { cos, sin, -sin, cos };
 
-    // Update is called once per frame
-    void Update()
+    public static readonly Dictionary<Execs, Vector2Int[]> Cells = new Dictionary<Execs, Vector2Int[]>()
     {
-        
-    }
+        { Execs.Ruidger_I, new Vector2Int[] { new Vector2Int(-1, 1), new Vector2Int( 0, 1), new Vector2Int( 1, 1), new Vector2Int( 2, 1) } },
+        { Execs.Mikey_J, new Vector2Int[] { new Vector2Int(-1, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Execs.Lauren_L, new Vector2Int[] { new Vector2Int( 1, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Execs.KC_O, new Vector2Int[] { new Vector2Int( 0, 1), new Vector2Int( 1, 1), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Execs.Dean_S, new Vector2Int[] { new Vector2Int( 0, 1), new Vector2Int( 1, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0) } },
+        { Execs.Lucas_T, new Vector2Int[] { new Vector2Int( 0, 1), new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+        { Execs.Logan_Z, new Vector2Int[] { new Vector2Int(-1, 1), new Vector2Int( 0, 1), new Vector2Int( 0, 0), new Vector2Int( 1, 0) } },
+    };
 }
