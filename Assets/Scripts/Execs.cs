aefc4b345f3669281f64b1d04cfdf7b8abf64848
Jayden Deller-Daoust <jayden.dellerdaoust@gmail.com>
2024-11-01 20:23:45 -0400

    Stuff

---
+++ Assets/Scripts/Execs.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum Execs
+{
+    Lauren_L,
+    Ruidger_I,
+    Lucas_T,
+    Logan_Z,
+    Dean_S,
+    KC_O,
+    Mikey_J
+}
+
+[System.Serializable]
+public struct ExecData
+{
+    public Execs exec;
+    public Tile[] tiles;
+    public Vector2Int[] cells;
+}
